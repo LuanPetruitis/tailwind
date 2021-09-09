2ad5dd5771e67d61601b64bd4f34ed758fbe709b
LuanPetruitis <luanpetruitis@hotmail.com>
2021-09-09 19:24:49 -0300

    Forms

---
+++ dark_mode/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  // or 'media' -> paga do navegador or 'class' -> pega da tag html se tem a classe dark
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        amber: colors.amber,
+        rose: colors.rose
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+  ],
+}
